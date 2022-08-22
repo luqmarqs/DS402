ca993a5f769a1f4871554441067e21265a0d7255
Lucas Marques <66765396+luqmarqs@users.noreply.github.com>
2022-08-22 17:07:38 -0300

    update componente exemplo01 com o componente verificaidade

--- exemplos/src/components/Exemplo01.js
+++ exemplos/src/components/Exemplo01.js
@@ -1,5 +1,8 @@
+import VerificaIdade from "./VerificaIdade";
 
 export default function Exemplo01(props) {
+    let idadeIdeal = props.idade >= 18 ? props.idade : 18;
+
     return (
         <div>
             <h1>Primeiro componente!</h1>
@@ -7,6 +10,10 @@ export default function Exemplo01(props) {
                 Exemplo de componente com React.
                 Bem vindo, {props.nome}!
             </p>
+            <p>
+                Idade para dirigir: { idadeIdeal }
+            </p>
+            <VerificaIdade nome={ props.nome } idade={ props.idade } />
         </div>
 
         );
