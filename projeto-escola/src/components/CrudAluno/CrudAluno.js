cfa613124cf8cacfad0ffc14600d1cad6a6612de
Lucas Marques <luq.marqs@gmail.com>
2022-09-16 20:54:32 -0300

    Implementando inserção de alunos no front

--- projeto-escola/src/components/CrudAluno/CrudAluno.js
+++ projeto-escola/src/components/CrudAluno/CrudAluno.js
@@ -5,7 +5,7 @@ import Main from '../template/Main';
 
 const title = "Cadastro de Alunos";
 
-const urlAPI = "http://localhost:5176/api/aluno";
+const urlAPI = "https://localhost:7056/api/aluno";
 const initialState = {
     aluno: { id: 0, ra: '', nome:'', codCurso: 0},
     lista: []
@@ -19,6 +19,82 @@ export default class CrudAluno extends Component {
             this.setState({ lista: resp.data })
         })
     }
+
+    limpar(){
+        this.setState({ aluno: initialState.aluno});
+    }
+
+    salvar(){
+        const aluno = this.state.aluno;
+        aluno.codCurso = Number(aluno.codCurso);
+        const metodo = 'post';
+
+        axios[metodo](urlAPI, aluno)
+            .then(resp => {
+                const lista = this.getListaAtualizada(resp.data)
+                this.setState({ aluno: initialState.aluno, lista})
+            })
+    }
+
+    getListaAtualizada(aluno) {
+        const lista = this.state.lista.filter(a => a.id !== aluno.id);
+        lista.unshift(aluno);
+        return lista;
+    }
+
+    atualizaCampo(event) {
+        //clonar usuário a partir do state, para não alterar o state diretamente
+        const aluno = { ...this.state.aluno };
+        //usar o atributo NAME do input para identificar o campo a ser atualizado
+        aluno[event.target.name] = event.target.value;
+        //atualizar o state
+        this.setState({ aluno });
+    }
+
+    renderForm() {
+        return(
+            <div className="inclui-container">
+
+                <label> RA: </label>
+                <input
+                    type="text"
+                    id="ra"
+                    placeholder="RA do aluno"
+                    className="form-input"
+                    name="ra"
+                    value={this.state.aluno.ra}
+                    onChange={ e => this.atualizaCampo(e)}
+                />
+                <label> Nome: </label>
+                <input
+                    type="text"
+                    id="nome"
+                    placeholder="Nome do aluno"
+                    className="form-input"
+                    name="nome"
+                    value={this.state.aluno.nome}
+                    onChange={ e => this.atualizaCampo(e)}
+                />
+                <label> Código do Curso: </label>
+                <input
+                    type="number"
+                    id="codCurso"
+                    placeholder="0"
+                    className="form-input"
+                    name="codCurso"
+                    value={this.state.aluno.codCurso}
+                    onChange={ e => this.atualizaCampo(e)}
+                />
+
+                <button className="btnSalvar"
+                    onClick={ e => this.salvar(e) } > Salvar </button>
+
+                <button className="btnCancelar"
+                    onClick={ e => this.limpar(e) } > Cancelar </button>
+            </div>
+        )
+    }
+
     renderTable() {
         return (
             <div className="listagem">
@@ -47,6 +123,7 @@ export default class CrudAluno extends Component {
     render() {
         return (
             <Main title={title}>
+                {this.renderForm()}
                 {this.renderTable()}
             </Main>
         )
